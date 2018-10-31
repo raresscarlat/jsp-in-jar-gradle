95c74da0b3550ff25bd5e17e71c1db4d4e09b0f0
Rares  Scarlat <rares.scarlat@softvision.ro>
2018-10-31 16:40:32 +0200

    functional

---
+++ src/main/java/com/example/jspjargradle/JspjargradleApplication.java
@@ -0,0 +1,12 @@
+package com.example.jspjargradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspjargradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspjargradleApplication.class, args);
+	}
+}
